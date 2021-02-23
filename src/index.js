6dac08b465e57a0299a58a7dd0b406ca24ff6e43
ural1493 <ural1493@gmail.com>
2021-02-23 23:19:43 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+        let config = bracketsConfig.map((x)=> x.join(''));
+        console.log(config);
+        for(let i=0;i<config.length;){
+            if (str.includes(config[i])) {
+                str = str.replace(config[i], '');
+                i=0;
+                continue;
+            }
+            i++
+
+        }
+        return str.length==0 ? true : false;
 }
+
